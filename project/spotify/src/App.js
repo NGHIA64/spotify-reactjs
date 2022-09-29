edd24e2a154ab7c1cf49b739aaa9638f89be240b
NGHIA64 <83792258+NGHIA64@users.noreply.github.com>
2022-09-29 10:53:45 +0700

    done spotify

--- project/spotify/src/App.js
+++ project/spotify/src/App.js
@@ -5,17 +5,25 @@ import DetailSong from './compoments/DetailSong';
 import ListSongs from './compoments/ListSongs';
 import DataSongs from './data/songs.json'
 import { Songs } from './Context';
+import Playing from './compoments/Playing';
+import { useState } from 'react';
 function App() {
+  const [song, setSong] = useState(DataSongs[0])
+  const handleSetSong = (idSong) => {
+    const song = DataSongs.find(song => song.id === idSong)
+    setSong(song)
+  }
   return (
     <div className='App'>
-      <Songs.Provider value={{ DataSongs }}>
+      <Songs.Provider value={{ DataSongs, song , handleSetSong}}>
       <Navbar/>
-      <div className='grid grid-cols-3 bg-slate-700 h-screen-navbar-player'>
+      <div className='grid grid-cols-3 bg-slate-700 h-screen-navbar-player overflow-hidden'>
         {/* span 1 */}
         <DetailSong/>
         {/* span 2 */}
         <ListSongs/>
       </div>
+      <Playing/>
       </Songs.Provider>
     </div>
   );
--- project/spotify/src/compoments/ListSongs.js
+++ project/spotify/src/compoments/ListSongs.js
@@ -1,10 +1,23 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Songs } from '../Context';
 export default function ListSongs() {
-    const {DataSongs} = useContext(Songs)
+    const {DataSongs, handleSetSong, song} = useContext(Songs)
+    const [idSong, setidSong] = useState(0)
+    const handlePlaySong = (idSong) => {
+        setidSong(idSong)
+        handleSetSong(idSong)
+    }
+    useEffect(() => {
+        if(!song){
+            setidSong(DataSongs[0])
+        }else{
+            setidSong(song.id)
+        }
+        
+    },[song])
   return (
-    <div className="col-span-2">
-      <table className="table-auto w-full">
+    <div className="col-span-2 overflow-y-scroll">
+      <table className="table-auto w-full ">
         <thead className="text-white h-12">
           <tr>
             <th>#</th>
@@ -13,13 +26,16 @@ export default function ListSongs() {
             <th><i class="fas fa-download"></i></th>
           </tr>
         </thead>
-        <tbody>
-            <tr>
-                <th className="text-center">1</th>
-                <th className="text-left">1</th>
-                <th className="text-center">1</th>
-                <th className="text-center">1</th>
-            </tr>
+        <tbody >
+            {DataSongs.map((song, index) => 
+            <tr key={index} className={`bg-slate-800 h-12 text-gray-500 hover:bg-slate-600 ${idSong === song.id && 'bg-slate-600 text-teal-400 '}`} onClick={() => handlePlaySong(song.id)}>
+            <th className="text-center">{index + 1}</th>
+            <th className="text-left">{song.name}</th>
+            <th className="text-center">{song.author}</th>
+            <th className="text-center"><a href={song.url} ><i className="fas fa-download"></i></a></th>
+        </tr>
+        )}
+            
         </tbody>
       </table>
     </div>
