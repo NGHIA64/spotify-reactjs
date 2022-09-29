a27a0ac1f651864ce1fb8aa34895e1511adc2d15
NGHIA64 <83792258+NGHIA64@users.noreply.github.com>
2022-09-29 08:41:44 +0700

    import list songs

--- project/spotify/src/App.js
+++ project/spotify/src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import Navbar from './compoments/Navbar';
 import DetailSong from './compoments/DetailSong';
 import ListSongs from './compoments/ListSongs';
-
+import DataSongs from './data/songs.json'
+import { Songs } from './Context';
 function App() {
   return (
-    <div className='Ap'>
+    <div className='App'>
+      <Songs.Provider value={{ DataSongs }}>
       <Navbar/>
       <div className='grid grid-cols-3 bg-slate-700 h-screen-navbar-player'>
         {/* span 1 */}
@@ -14,6 +16,7 @@ function App() {
         {/* span 2 */}
         <ListSongs/>
       </div>
+      </Songs.Provider>
     </div>
   );
 }
--- project/spotify/src/compoments/ListSongs.js
+++ project/spotify/src/compoments/ListSongs.js
@@ -1,9 +1,27 @@
-import React from "react";
-
-export default function ListSongs(){
-    return (
-        <div className="col-span-2">
-            b
-        </div>
-    )
+import React, { useContext } from "react";
+import { Songs } from '../Context';
+export default function ListSongs() {
+    const {DataSongs} = useContext(Songs)
+  return (
+    <div className="col-span-2">
+      <table className="table-auto w-full">
+        <thead className="text-white h-12">
+          <tr>
+            <th>#</th>
+            <th className="text-left">Title</th>
+            <th className="w-[10%]">Author</th>
+            <th><i class="fas fa-download"></i></th>
+          </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th className="text-center">1</th>
+                <th className="text-left">1</th>
+                <th className="text-center">1</th>
+                <th className="text-center">1</th>
+            </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 }
